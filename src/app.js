921cd26c466fecd7f90403d67e17d4f6df9bbedc
Marty Kane <marty@pixotech.com>
2016-12-19 16:43:46 -0600

    Add some controls and a home-brewed redux

--- src/app.js
+++ src/app.js
@@ -6,11 +6,31 @@ newGame(10);
 
 function action(actionName, actionProperties) {
     console.log('ACTION ' + actionName, actionProperties);
+    switch ( actionName ) {
+        case 'NEW':
+            console.log('new game with size ' + actionProperties.size);
+            newGame(1 * actionProperties.size);
+            break;
+        case 'NEXT':
+            console.log('compute next board state');
+            states.push(game.tick(latestState()));
+            render();
+            break;
+        case 'UNDO':
+            states.pop();
+            render();
+            break;
+        default:
+            console.log('Unknown action ' + actionName, actionProperties);
+    }
+}
+
+function latestState() {
+    return states[states.length-1];
 }
 
 function render() {
-    const state = states[states.length-1];
-    console.log(`render `,state);
+    const state = latestState();
     if (state)
         ReactDOM.render(<Board state={state} action={action}></Board>, boardElement);
     ReactDOM.render(<Controls states={states} action={action}></Controls>, controlsElement);
